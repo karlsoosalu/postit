872d8d0bbddbc75096ab6f389ce20aac666e352c
MarkMarten <martenmark@hotmail.com>
2020-11-22 21:17:12 +0200

    almost completed

--- src/components/Browse.vue
+++ src/components/Browse.vue
@@ -3,8 +3,19 @@
     <section class="browse-container">
     <div class="profiles-container">
       <Header />
-      <p>
-      </p>
+      <div class=profile v-for="profile in profiles" :key="profile.id">
+        <img v-bind:src="profile.avatar" alt="Profile author" />
+        <div class=profile-name >
+          {{ profile.firstname }}
+          {{ profile.lastname }}
+        </div>
+        <button type=button name=follow class=follow-button>
+          Follow
+        </button>
+        
+
+      </div>
+      
     </div>
     </section>
   </div>
@@ -44,6 +55,56 @@ export default {
     margin: auto auto;
     padding: 90px 15px 15px 15px;
     background-color: #ffffff;
+}
+
+.profile {
+  width: 40%;
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+  border: 1px solid lightgrey;
+  border-radius: 15px;
+}
+
+.profile img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: top center;
+}
+
+.profile-name {
+  margin-top: 15px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 22px;
+}
+
+.follow-button {
+  background: #82008F;
+  border: 1px solid #82008F;
+  font-size: 20px;
+}
+
+.follow-button.followed {
+  background: white;
+  color: black;
+  box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
+}
+
+button {
+    padding: 8px 16px;
+    margin: 4px 0;
+    color: #ffffff;
+    background-color: #01579b;
+    border: none;
+    border-radius: 4px;
+}
+
+button:hover {
+    box-shadow: 0 0 5px rgba(38, 50, 56, 0.7);
+    cursor: pointer;
 }
 
 </style>
